dc75497f9ec6ab2dac2cded094da655845cfeb3d
sukemaru-injp <72376331+sukemaru-injp@users.noreply.github.com>
2022-02-27 20:21:14 +0900

    action

--- components/templates/AuthCheck.tsx
+++ components/templates/AuthCheck.tsx
@@ -1,12 +1,31 @@
 import { FC, ReactNode, useEffect } from 'react'
+import { useRecoilValue } from 'recoil'
+import { authState, Auth } from '../../store/auth/atom'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from '../../libs/Firebase'
+import { getUserInfoByFireStore } from '../../utils/auth'
 
 interface Props {
   children: ReactNode
 }
 
 const AuthCheck: FC<Props> = (props: Props) => {
+  const { isLogin }: Auth = useRecoilValue(authState)
   useEffect(() => {
-    console.log('auth')
+    if (isLogin) { return }
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const { uid } = user
+        try {
+          await getUserInfoByFireStore(uid)
+        } catch (e) {
+          // eslint-disable-next-line no-console
+          console.error(e)
+        }
+      }
+      // eslint-disable-next-line no-console
+      console.log('auth', user)
+    })
   }, [])
   return (
     <>
