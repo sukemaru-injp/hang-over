a923f13c3063ed6e49d8649987ab6354fd89549e
sukemaru-injp <72376331+sukemaru-injp@users.noreply.github.com>
2022-03-04 10:29:48 +0900

    nav_style

--- components/organisms/Nav.tsx
+++ components/organisms/Nav.tsx
@@ -1,23 +1,30 @@
 import { VFC } from 'react'
 import styles from './styles/Nav.module.scss'
 import Link from 'next/link'
+import Card from '../atoms/Card'
 
 interface Props {}
 const Nav: VFC<Props> = () => {
   return (
     <>
-      <nav>
-        <ul>
-          <li className={styles.Nav__list}>
-            <Link passHref href="/">
-              <a>ユーザー管理</a>
-            </Link>
-          </li>
-          <li className={styles.Nav__list}>
-            <p>ログアウト</p>
-          </li>
-        </ul>
-      </nav>
+      <Card>
+        <nav className={styles.Nav}>
+          <ul>
+            <li className={styles.Nav__list}>
+              <button className={styles.Nav__button}>
+                <Link passHref href="/">
+                  <a>ユーザー管理</a>
+                </Link>
+              </button>
+            </li>
+            <li className={styles.Nav__list}>
+              <button className={styles.Nav__button}>
+              ログアウト
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </Card>
     </>
   )
 }
