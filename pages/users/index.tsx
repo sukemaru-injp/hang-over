4a04c3af4cc9cd650525bba5ccd49f96d9758a34
sukemaru-injp <72376331+sukemaru-injp@users.noreply.github.com>
2022-03-04 14:34:04 +0900

    add_lodash

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,26 +1,41 @@
 import type { NextPage } from 'next'
+import { useState } from 'react'
 import { firestore } from '../../libs/Firebase'
 import { getDocs, collection } from 'firebase/firestore'
 import { getPrefix } from '../../src/misc'
 import { useEffect } from 'react'
+import { map } from 'lodash'
+
+interface UserData {
+  email: string,
+  name: string,
+  uid: string,
+  createDate: any
+}
 
 const UsersPage: NextPage = () => {
+  const [users, setUsers] = useState<UserData[]>([])
   useEffect(() => {
     (async() => {
       try {
         const res = await getDocs(collection(firestore, `${getPrefix()}users`))
         res.forEach(v => {
-          console.log(v.data())
+          const user = v.data() as UserData
+          setUsers([...users, user])
         })
       } catch (e) {
         // eslint-disable-next-line no-console
         console.error(e)
-      }  
+      }
     })()
   }, [])
+  console.log(users)
 
   return (
     <>
+      {map(users, (v, id) => {
+        return (<p key={id}>{v.name}</p>)
+      })}
     </>
   )
 }
