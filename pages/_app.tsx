f2f5c7a736d184646c77144832fe2c114b100141
sukemaru-injp <72376331+sukemaru-injp@users.noreply.github.com>
2022-03-09 16:18:09 +0900

    fc

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { FC } from 'react'
 import { RecoilRoot } from 'recoil'
 import DefaultView from '../components/templates/DefaultView'
 import AuthProvider from '../components/provider/AuthProvider'
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
+const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <RecoilRoot>
