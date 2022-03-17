a36cb9e763f9fc733c3b9cc7189fd27784c61d88
sukemaru-injp <72376331+sukemaru-injp@users.noreply.github.com>
2022-03-17 12:16:43 +0900

    uuid

--- components/templates/NewRestaurant.tsx
+++ components/templates/NewRestaurant.tsx
@@ -1,12 +1,19 @@
 import { FC, useState } from 'react'
 import styles from './styles/NewRestaurant.module.scss'
 import InputAndLabel from '../molecules/InputAndLabel'
+import Button from '../atoms/Button'
+import { v4 as uuidv4 } from 'uuid'
 
 interface Props {}
 const NewRestaurant: FC<Props> = () => {
   const [name, setName] = useState('')
   const [station, setStationName] = useState('')
   const [tel, setTel] = useState('')
+
+  const onSubmit = () => {
+    const id = uuidv4()
+    console.log(id.split('-'))
+  }
 
   return (
     <>
@@ -33,6 +40,12 @@ const NewRestaurant: FC<Props> = () => {
             placeholder='09010101004'
             value={tel}
             onChange={(e) => setTel(e.target.value)} />
+        </div>
+        <div className={styles.NewRestaurant__buttonArea}>
+          <Button
+            onClick={() => onSubmit()}>
+              作成する
+          </Button>
         </div>
       </div>
     </>
