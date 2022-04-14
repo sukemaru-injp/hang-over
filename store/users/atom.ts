c4dad6256bb28a3381119b7114af57228f5cd29f
sukemaru-injp <72376331+sukemaru-injp@users.noreply.github.com>
2022-04-14 13:11:09 +0900

    localStrageにもたなくても良い情報

--- store/users/atom.ts
+++ store/users/atom.ts
@@ -1,11 +1,7 @@
 import { atom } from 'recoil'
 import type { UserData } from './types'
-import { recoilPersist } from 'recoil-persist'
-
-const { persistAtom } = recoilPersist()
 
 export const userList = atom<UserData[]>({
   key: 'usersState',
   default: [],
-  effects_UNSTABLE: [persistAtom]
 })
--- store/yakitori/atom.ts
+++ store/yakitori/atom.ts
@@ -1,11 +1,7 @@
 import { atom } from 'recoil'
 import type { YakitoriInfo } from './types'
-import { recoilPersist } from 'recoil-persist'
-
-const { persistAtom } = recoilPersist()
 
 export const userList = atom<YakitoriInfo[]>({
   key: 'yakitoriState',
   default: [],
-  effects_UNSTABLE: [persistAtom]
 })
