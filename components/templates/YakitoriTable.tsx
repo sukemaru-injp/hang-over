d74da83273266e1bdd40a8d7fa74f60356efd896
sukemaru-injp <72376331+sukemaru-injp@users.noreply.github.com>
2022-04-17 15:54:16 +0900

    add_button

--- components/templates/YakitoriTable.tsx
+++ components/templates/YakitoriTable.tsx
@@ -3,6 +3,7 @@ import styles from './styles/YakitoriTable.module.scss'
 import { map } from 'lodash'
 import Card from '../atoms/Card'
 import { YakitoriInfoType } from '../../store/yakitori/types'
+import Button from '../atoms/Button'
 
 interface Props {
   list: YakitoriInfoType[]
@@ -10,18 +11,41 @@ interface Props {
 const YakitoriTable: VFC<Props> = (props: Props) => {
   const HEADER_LABELS: string[] = ['店名', '住所', '操作']
   console.log('props', props)
+  const onClickDelete = (item: YakitoriInfoType) => {
+    console.log('delete', item)
+  }
+
   return (
     <Card>
-      <table className={styles.UserTable}>
+      <table className={styles.YakitoriTable}>
         <thead>
           <tr>
             {map(HEADER_LABELS, (v, idx) => {
               return (
-                <th key={`head${idx}`} className={styles.UserTable__head}>{v}</th>
+                <th key={`head${idx}`} className={styles.YakitoriTable__head}>{v}</th>
               )
             })}
           </tr>
         </thead>
+        <tbody>
+          {map(props.list, (item, idx) => {
+            return (
+              <tr key={`item${idx}`}>
+                <td className={styles.YakitoriTable__content}>{item.name}</td>
+                <td className={styles.YakitoriTable__content}>{item.address}</td>
+
+                <td className={styles.UserTable__content}>
+                  <Button
+                    color='delete'
+                    type='button'
+                    onClick={() => onClickDelete(item)}>
+                      削除
+                  </Button>
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>
       </table>
     </Card>
   )
